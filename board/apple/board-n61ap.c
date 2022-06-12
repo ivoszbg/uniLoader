94b2cf0c4ac4316413b7af00bb1250f3e2e9b7d6
ivoszbg <ivo.ivanov@null.net>
2022-06-12 19:08:34 +0300

    board: apple: Add basic iPhone 6 support
    Signed-off-by: Ivaylo Ivanov <ivo.ivanov.ivanov1@gmail.com>

---
+++ board/apple/board-n61ap.c
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2022, Ivaylo Ivanov <ivo.ivanov.ivanov1@gmail.com>
+ */
+
+#include <board/board-n61ap.h>
+
+void board_init(void) {
+
+}
---
+++ include/board/board-n61ap.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2022, Ivaylo Ivanov <ivo.ivanov.ivanov1@gmail.com>
+ */
+
+#ifndef BOARD_N61AP_H_	/* Include guard */
+#define BOARD_N61AP_H_
+
+/* TODO: Adjust PAYLOAD_ENTRY */
+#define PAYLOAD_ENTRY		0x830000000
+#define PAYLOAD_SIZE		0x2000000
+
+#endif // BOARD_N61AP_H_
