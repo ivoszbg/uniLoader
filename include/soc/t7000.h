abb3a2cf4af02ef84c9898dbd4c5af9354521d93
ivoszbg <ivo.ivanov@null.net>
2022-06-12 17:55:59 +0300

    soc: apple: Add basic support for Apple T7000 (aka A8)
    Signed-off-by: Ivaylo Ivanov <ivo.ivanov.ivanov1@gmail.com>

---
+++ include/soc/t7000.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2022, Ivaylo Ivanov <ivo.ivanov.ivanov1@gmail.com>
+ */
+
+#ifndef T7000_H_	/* Include guard */
+#define T7000_H_
+
+#endif // T7000_H_
---
+++ soc/apple/t7000.c
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2022, Ivaylo Ivanov <ivo.ivanov.ivanov1@gmail.com>
+ */
+
+#include <soc/t7000.h>
+
+void soc_init(void) {
+
+}
