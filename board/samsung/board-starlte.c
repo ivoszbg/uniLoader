aa70ec5f42eabb6066e84a6a2da4da0a7d2cf424
Markuss Broks <markuss.broks@gmail.com>
2022-06-13 19:03:54 +0300

    board: starlte: Add support for Samsung Galaxy S9
    Signed-off-by: Markuss Broks <markuss.broks@gmail.com>

---
+++ board/samsung/board-starlte.c
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2022, Markuss Broks <markuss.broks@gmail.com>
+ */
+
+#include <board/board-starlte.h>
+
+void board_init(void) {
+
+}
---
+++ include/board/board-starlte.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2022, Markuss Broks <markuss.broks@gmail.com>
+ */
+
+#ifndef BOARD_STARLTE_H   /* Include guard */
+#define BOARD_STARLTE_H
+
+#define PAYLOAD_ENTRY		0x90000000
+
+#endif // BOARD_STARLTE_H
