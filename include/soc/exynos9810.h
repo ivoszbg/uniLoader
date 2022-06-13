6d5d964955965741af73077c3a8cd1bbc2eaa99d
Markuss Broks <markuss.broks@gmail.com>
2022-06-13 19:03:35 +0300

    soc: exynos: Add basic support for Exynos 9810
    Signed-off-by: Markuss Broks <markuss.broks@gmail.com>

---
+++ include/soc/exynos9810.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2022, Markuss Broks <markuss.broks@gmail.com>
+ */
+
+#ifndef EXYNOS9810_H_	/* Include guard */
+#define EXYNOS9810_H_
+
+#define DECON_F_BASE		0x16030000
+#define HW_SW_TRIG_CONTROL	0x70
+
+#endif // EXYNOS9810_H_
