4d1a5f84bcaca61c9c07328d5a659f62296d6b62
ivoszbg <ivo.ivanov@null.net>
2022-06-14 18:51:04 +0300

    board: Drop Board specific headers
    It isn't required anymore, as we've moved on to Kconfig.
    
    Signed-off-by: Ivaylo Ivanov <ivo.ivanov.ivanov1@gmail.com>

--- board/apple/board-n61ap.c
+++ board/apple/board-n61ap.c
@@ -3,8 +3,6 @@
  * Copyright (c) 2022, Ivaylo Ivanov <ivo.ivanov.ivanov1@gmail.com>
  */
 
-#include <board/board-n61ap.h>
-
 void board_init(void) {
 
 }
--- board/samsung/board-dreamlte.c
+++ board/samsung/board-dreamlte.c
@@ -3,8 +3,6 @@
  * Copyright (c) 2022, Ivaylo Ivanov <ivo.ivanov.ivanov1@gmail.com>
  */
 
-#include <board/board-dreamlte.h>
-
 void board_init(void) {
 
 }
