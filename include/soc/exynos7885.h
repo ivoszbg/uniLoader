989a2ad133b752c1b77fbf36de433208dd6a1f39
David Wronek <w.david0@protonmail.com>
2022-06-15 08:21:37 +0200

    soc: exynos: Add basic support for Exynos 7885
    Signed-off-by: David Wronek <w.david0@protonmail.com>

---
+++ include/soc/exynos7885.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2022, David Wronek <w.david0@protonmail.com>
+ */
+
+#ifndef EXYNOS7885_H_	/* Include guard */
+#define EXYNOS7885_H_
+
+#define DECON_F_BASE		0x14860000
+#define HW_SW_TRIG_CONTROL	0x70
+
+#endif // EXYNOS7885_H_
