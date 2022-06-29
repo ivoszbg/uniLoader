1f62b4e5ac62889e0524e5666bd9750d70f843cd
Faiz Faadhillah <faiz.faadhillah@gmail.com>
2022-06-29 12:59:54 +0700

    soc: exynos: Add basic support for Exynos 7420
    Signed-off-by: Faiz Faadhillah <faiz.faadhillah@gmail.com>

---
+++ include/soc/exynos7420.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2022, Faiz Faadhillah <faiz.faadhillah@gmail.com>
+ */
+
+#ifndef EXYNOS7420_H_	/* Include guard */
+#define EXYNOS7420_H_
+
+#define DECON_F_BASE		0x13930000
+#define HW_SW_TRIG_CONTROL	0x6b0
+
+#endif // EXYNOS7420_H_
