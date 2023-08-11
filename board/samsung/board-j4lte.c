9d2a29835cceae3e1aa9e5f44e0d383ef691f1fc
predefine <predefine@yandex.ru>
2023-08-12 00:37:20 +0700

    add Samsung Galaxy J4lte & Exynos 7570 support

---
+++ board/samsung/board-j4lte.c
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2023, predefine <predefine@yandex.ru>
+ */
+
+void board_init(void) {
+
+}
---
+++ include/soc/exynos7570.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2023, predefine <predefine@yandex.ru>
+ */
+
+#ifndef EXYNOS7570_H_	/* Include guard */
+#define EXYNOS7570_H_
+
+#define DECON_F_BASE		0x14830000
+#define HW_SW_TRIG_CONTROL	0x70
+
+#endif // EXYNOS7570_H_
