7ed7b2b90232bc847c08e5433b0e3fe6167c1d7d
Ivaylo Ivanov <ivoteraria@gmail.com>
2023-08-15 14:15:02 +0300

    soc: apple: Add basic support for Apple T8010 (aka A10)
    Signed-off-by: Ivaylo Ivanov <ivo.ivanov.ivanov1@gmail.com>

---
+++ include/soc/t8010.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2023, Ivaylo Ivanov <ivo.ivanov.ivanov1@gmail.com>
+ */
+
+#ifndef T8010_H_	/* Include guard */
+#define T8010_H_
+
+#endif // T8010_H_
---
+++ soc/apple/t8010.c
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2023, Ivaylo Ivanov <ivo.ivanov.ivanov1@gmail.com>
+ */
+
+#include <soc/t8010.h>
+
+void soc_init(void) {
+
+}
