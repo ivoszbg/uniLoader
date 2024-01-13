e330e60fb99a58a7fcb95cb64b2765f68e2a4846
BotchedRPR <thenxguy0@gmail.com>
2024-01-13 23:13:10 +0000

    samsung-keys: rewrite the entire keys code xD

--- lib/keys/keys.c
+++ lib/keys/keys.c
@@ -2,39 +2,71 @@
 // 2024 - BotchedRPR <ibelwon@protonmail.com>
 
 #include <main.h>
-#include <string.h>
 
+#define POWER_BUTTON 0
+#define VOLUMEDOWN_BUTTON 1
+#define VOLUMEUP_BUTTON 2
+
+/* Don't call this function. */
 int readKey(int base, int bank) {
-  unsigned int address = ((base + bank) + 0x4);
-  return ((char *)address)[0];
+    unsigned int address = ((base + bank) + 0x4);
+    return ((unsigned char *)address)[0];
+}
+
+/* Call this one instead! :)*/
+int checkKey(int button)
+{
+    switch(button)
+    {
+        case POWER_BUTTON:
+            if (!(readKey(0x15850000, 0x40) & (1 << 0x4))) {
+                return 1;
+            }
+        
+        case VOLUMEDOWN_BUTTON:
+            if (!(readKey(0x15850000, 0x0) & (1 << 0x4))) {
+                return 1;
+            }
+
+        case VOLUMEUP_BUTTON:
+            if (!(readKey(0x15850000, 0x00) & (1 << 0x3))) {
+                return 1;
+            }
+
+        default:
+            return 0;
+    }
+    return 0;
+}
+
+int anyKeyPressed()
+{
+    /* This code looks (and is) very hacky but it works. */
+    if(checkKey(POWER_BUTTON) ||
+        checkKey(VOLUMEDOWN_BUTTON) ||
+        checkKey(VOLUMEUP_BUTTON))
+    {
+        return 1;
+    }
+    return 0;
 }
 
 void testreadkeys() {
-  int isPressed = 0;
+    int isPressed = 0;
 
-  while (1) {
-    if (isPressed == 0) {
-      if (!(readKey(0x15850000, 0x40) & (1 << 0x4))) {
-        printk("POWER 0 pressed");
-        isPressed = 1;
-      }
-
-      if (!(readKey(0x15850000, 0x0) & (1 << 0x4))) {
-        printk("VOL- 1 pressed");
-        isPressed = 1;
-      }
-
-      if (!(readKey(0x15850000, 0x00) & (1 << 0x3))) {
-        printk("VOL+ 2 pressed");
-        isPressed = 1;
-      }
-    } else {
-      // Poll all keys and check if non are pressed
-      if ((readKey(0x15850000, 0x40) & (1 << 0x4)) &&
-          (readKey(0x15850000, 0x0) & (1 << 0x4)) &&
-          (readKey(0x15850000, 0x00) & (1 << 0x3))) {
-        isPressed = 0;
-      }
+    while(1)
+    {
+        if(isPressed == 0)
+        {
+            if(anyKeyPressed())
+            {
+                printk("You have pressed a key. Good job.");
+                isPressed = 1;
+            }
+        }
+        else if(!anyKeyPressed())
+        {
+            isPressed = 0;
+        }
     }
-  }
 }
