ab1e1fdc8f334156104def619eabe91e8c70280d
BotchedRPR <thenxguy0@gmail.com>
2024-01-14 13:56:32 +0000

    halal dont mess with this ever again

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 set -e
 make clean
-make x1s_defconfig
 make -j1
-cp uniLoader AIK-Linux/split_img/boot.img-kernel
+cp uniLoader AIK-Linux/split_img/boot-kernel
 cd AIK-Linux
 ./repackimg.sh
-cp unsigned-new.img ../boot.img
 cd ..
 echo ***BUILT***
