2b2eabdfbe1e71f65b55761886196512ed4f201c
halal-beef <78730004+halal-beef@users.noreply.github.com>
2024-02-08 10:10:51 +0000

    initial APQ8064 and Galaxy S4 (JFLTE) support
    also fix c1s_defconfig perms

---
+++ include/soc/apq8064.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2024, halal-beef <78730004+halal-beef@users.noreply.github.com>
+ */
+
+#ifndef APQ8064_H_	/* Include guard */
+#define APQ8064_H_
+
+#endif // APQ8064_H_
