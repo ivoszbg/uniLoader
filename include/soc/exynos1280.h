20f759082d918c0ae7550efe856c14060a1678cc
Muhammad <thevancedgamer@mentallysanemainliners.org>
2025-04-04 16:43:32 +0300

    board: Add support for Samsung Galaxy A33
    Adds basic support for the Exynos 1280 SoC in the Galaxy A33

---
+++ include/soc/exynos1280.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2025, Muhammad <thevancedgamer@mentallysanemainliners.org>
+ */
+
+#ifndef EXYNOS1280_H_	/* Include guard */
+#define EXYNOS1280_H_
+
+#define DECON_F_BASE		0x14940000
+#define HW_SW_TRIG_CONTROL	0x30
+
+#endif // EXYNOS1280_H_
