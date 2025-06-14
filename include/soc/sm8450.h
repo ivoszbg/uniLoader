88e8cdabe84902672b635a2d34c5870b22f3fa26
Ghatto <ghatto404@gmail.com>
2025-06-14 04:25:43 +0000

    add missing files

---
+++ include/soc/sm8450.h
@@ -0,0 +1,6 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef SM8450_H_	/* Include guard */
+#define SM8450_H_
+
+#endif // SM8450_H_
---
+++ soc/qualcomm/sm8450.c
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#include <soc/sm8450.h>
+
+void soc_init(void) {
+	
+}
