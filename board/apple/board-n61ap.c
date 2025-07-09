2ceed988bc6e29b7a5461f01e3bbe24b8b7347ec
Ivaylo Ivanov <ivo.ivanov.ivanov1@gmail.com>
2025-07-09 11:39:41 +0300

    board: apple: modernize n61ap
    This brings it up to the latest changes, like the other boards,
    and makes it compilable.

--- board/apple/board-n61ap.c
+++ board/apple/board-n61ap.c
@@ -2,7 +2,48 @@
 /*
  * Copyright (c) 2022, Ivaylo Ivanov <ivo.ivanov.ivanov1@gmail.com>
  */
+#include <board.h>
+#include <drivers/framework.h>
+#include <lib/simplefb.h>
 
-void board_init(void) {
+void init_board_funcs(void *board)
+{
+	/*
+	 * Parsing the struct directly without restructing is
+	 * broken as of Sep 29 2024
+	 */
+	struct {
+		const char *name;
+		int ops[BOARD_OP_EXIT];
+	} *board_restruct = board;
 
+	board_restruct->name = "N61AP";
+}
+
+int board_init(void)
+{
+	return 0;
+}
+
+int board_late_init(void)
+{
+	return 0;
+}
+
+int board_driver_setup(void)
+{
+	struct {
+		int width;
+		int height;
+		int stride;
+		void *address;
+	} simplefb_data = {
+		.width = 752,
+		.height = 1334,
+		.stride = 4,
+		.address = (void *)0x83e900000
+	};
+
+	REGISTER_DRIVER("simplefb", simplefb_probe, &simplefb_data);
+	return 0;
 }
