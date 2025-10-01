f81ce6731ccd5c4513d72e86a44d8ffc4c0e56d2
Igor Belwon <igor.belwon@mentallysanemainliners.org>
2025-10-01 19:00:33 +0200

    board: qemu-virt: Remove stub init functions
    These do not accomplish anything, and with the new initcall logic,
    can be harmlessly removed.
    
    Signed-off-by: Igor Belwon <igor.belwon@mentallysanemainliners.org>

--- board/qemu/board-virt.c
+++ board/qemu/board-virt.c
@@ -38,22 +38,10 @@ int virt_init(void)
 	return 0;
 }
 
-int virt_late_init(void)
-{
-	return 0;
-}
-
-int virt_drv(void)
-{
-	return 0;
-}
-
 struct board_data board_ops = {
 	.name = "qemu-virt",
 	.ops = {
 		.early_init = virt_init,
-		.drivers_init = virt_drv,
-		.late_init = virt_late_init,
 	},
 	.quirks = 0
 };
