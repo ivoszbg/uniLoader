ivoszbg/uniLoader

commit 7993348878f9252da96ac5096d2dbb3727b328bc

main: Add proper initcall logic
Currently we are checking if the function returns nonzero. This is fine,
however it adds a lot of bloat, for example the late_init initcall is
practically never used and is only a stub function that returns zero. So
- add a proper initcall macro to prepare for cleaning up unused stubs.

Signed-off-by: Igor Belwon <igor.belwon@mentallysanemainliners.org>

main/main.c
@@ -12,11 +12,8 @@ extern struct board_data board_ops;
 
 void main(void* dt, void* kernel, void* ramdisk)
 {
-	if (board_ops.ops.early_init())
-		goto err;
-
-	if (board_ops.ops.drivers_init())
-		goto err;
+	INITCALL(board_ops.ops.early_init);
+	INITCALL(board_ops.ops.drivers_init);
 
 	printk(KERN_INFO, "             .__.____                     .___\n");
 	printk(KERN_INFO, " __ __  ____ |__|    |    _________     __| _/___________\n");
@@ -28,8 +25,7 @@ void main(void* dt, void* kernel, void* ramdisk)
 	printk(KERN_INFO, "passed board initialization\n");
 	printk(KERN_INFO, "welcome to uniLoader %s on %s\n", VER_TAG, board_ops.name);
 
-	if (board_ops.ops.late_init())
-		goto err;
+	INITCALL(board_ops.ops.drivers_init);
 
 	/* copy kernel to memory and boot  */
 	printk(KERN_INFO, "booting linux...\n");
@@ -43,9 +39,9 @@ void main(void* dt, void* kernel, void* ramdisk)
 	load_kernel_and_jump(0, 0, dt, (void*)CONFIG_PAYLOAD_ENTRY);
 #endif
 
- err:
 	/* we shouldn't get there */
 	/* TODO: make this a per-board reset */
 	printk(KERN_EMERG, "Something wrong happened, we shouldn't be here. Hanging....\n");
-	while(1) {}
+	HANG();
+
 }

include/main.h
@@ -11,6 +11,22 @@
 extern unsigned long kernel_size;
 extern unsigned long ramdisk_size;
 
+#define HANG() while(1) {}
+
+/*
+ * Some initcalls (for example late_init) are not used
+ * on any devices (other than a stub return function).
+ * As such, all initcalls are optional, and only ran if they exist.
+ * However, if they do exist, getting a positive return value is needed.
+ */
+#define INITCALL(_call) \
+	do { \
+		if (_call && _call()) { \
+			printk(KERN_EMERG, "initcall %s() failed\n", #_call); \
+			HANG(); \
+		} \
+	} while (0)
+
 #ifdef __aarch64__
 extern void load_kernel_and_jump(void* dtb, void* x1, void* x2, void* x3,
 				 void* kernel);

commit 512ae177ad1da749b0fcc6a6505a1fc936da1751

main: Fix duplicate drivers_init call
Commit https://github.com/ivoszbg/uniLoader/commit/7993348878f9252da96ac5096d2dbb3727b328bc accidentally replaced the late_init call with a
duplicate drivers_init call when converting the initcall logic to use
the new INITCALL macro.

This causes drivers to be probed twice during boot. For the simplefb
driver, this results in the framebuffer being cleared after the boot
logo has already been printed, since simplefb_probe() calls
clean_fbmem() unconditionally.

main/main.c
@@ -32,7 +32,7 @@ void main(void* dt, void* kernel, void* ramdisk)
 	printk(KERN_INFO, "passed board initialization\n");
 	printk(KERN_INFO, "welcome to uniLoader %s on %s\n", VER_TAG, board_ops.name);
 
-	INITCALL(board_ops.ops.drivers_init);
+	INITCALL(board_ops.ops.late_init);
 
 #ifdef CONFIG_LIBFDT
 	ret = ramdisk_handler_patch_dtb(dt, &fdt_buf, sizeof(fdt_buf));