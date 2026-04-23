c9439e7bba30588dd5038a454254f623dff7e71b
Spyware_Hunter <spyware_hunter@proton.me>
2026-04-23 20:46:42 +0000

    board: Correct codenames for some Samsung devices and minor text changes

--- board/samsung/board-a30.c
+++ board/samsung/board-a30.c
@@ -2,14 +2,16 @@
 /*
  * Copyright (c) 2022, David Wronek <w.david0@protonmail.com>
  * Copyright (c) 2024, Ivaylo Ivanov <ivo.ivanov.ivanov1@gmail.com>
+ * Copyright (c) 2026, faveoled <faveoled@yandex.com> 
  */
+
 #include <board.h>
 #include <drivers/framework.h>
 #include <lib/simplefb.h>
 #include <soc/exynos7885.h>
 
 // Early initialization
-int a105f_init(void)
+int a30_init(void)
 {
 	/* Allow framebuffer to be written to */
 	*(int*) (DECON_F_BASE + HW_SW_TRIG_CONTROL) = 0x1281;
@@ -17,28 +19,28 @@ int a105f_init(void)
 }
 
 #ifdef CONFIG_SIMPLE_FB
-static struct video_info a105f_fb = {
+static struct video_info a30_fb = {
 	.format = FB_FORMAT_ARGB8888,
-	.width = 720,
-	.height = 1520,
+	.width = 1080,
+	.height = 2340,
 	.stride = 4,
 	.address = (void *)0xec000000
 };
 #endif
 
-int a105f_drv(void)
+int a30_drv(void)
 {
 #ifdef CONFIG_SIMPLE_FB
-	REGISTER_DRIVER("simplefb", simplefb_probe, &a105f_fb);
+	REGISTER_DRIVER("simplefb", simplefb_probe, &a30_fb);
 #endif
 	return 0;
 }
 
 struct board_data board_ops = {
-	.name = "samsung-a105f",
+	.name = "samsung-a30",
 	.ops = {
-		.early_init = a105f_init,
-		.drivers_init = a105f_drv,
+		.early_init = a30_init,
+		.drivers_init = a30_drv,
 	},
 	.quirks = 0
 };
