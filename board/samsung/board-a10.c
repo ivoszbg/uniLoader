de66f74aa9e2cfb878bd8a2001856a82b2533015
Spyware_Hunter <spyware_hunter@proton.me>
2026-04-23 20:46:42 +0000

    board: Correct codenames for some Samsung devices and minor text changes

--- board/samsung/board-a10.c
+++ board/samsung/board-a10.c
@@ -2,16 +2,14 @@
 /*
  * Copyright (c) 2022, David Wronek <w.david0@protonmail.com>
  * Copyright (c) 2024, Ivaylo Ivanov <ivo.ivanov.ivanov1@gmail.com>
- * Copyright (c) 2026, faveoled <faveoled@yandex.com> 
  */
-
 #include <board.h>
 #include <drivers/framework.h>
 #include <lib/simplefb.h>
 #include <soc/exynos7885.h>
 
 // Early initialization
-int a305f_init(void)
+int a10_init(void)
 {
 	/* Allow framebuffer to be written to */
 	*(int*) (DECON_F_BASE + HW_SW_TRIG_CONTROL) = 0x1281;
@@ -19,28 +17,28 @@ int a305f_init(void)
 }
 
 #ifdef CONFIG_SIMPLE_FB
-static struct video_info a305f_fb = {
+static struct video_info a10_fb = {
 	.format = FB_FORMAT_ARGB8888,
-	.width = 1080,
-	.height = 2340,
+	.width = 720,
+	.height = 1520,
 	.stride = 4,
 	.address = (void *)0xec000000
 };
 #endif
 
-int a305f_drv(void)
+int a10_drv(void)
 {
 #ifdef CONFIG_SIMPLE_FB
-	REGISTER_DRIVER("simplefb", simplefb_probe, &a305f_fb);
+	REGISTER_DRIVER("simplefb", simplefb_probe, &a10_fb);
 #endif
 	return 0;
 }
 
 struct board_data board_ops = {
-	.name = "samsung-a305f",
+	.name = "samsung-a10",
 	.ops = {
-		.early_init = a305f_init,
-		.drivers_init = a305f_drv,
+		.early_init = a10_init,
+		.drivers_init = a10_drv,
 	},
 	.quirks = 0
 };
--- board/samsung/board-a12s.c
+++ board/samsung/board-a12s.c
@@ -9,14 +9,14 @@
 #include <soc/exynos3830.h>
 #include <stdint.h>
 
-int a127f_init(void)
+int a12s_init(void)
 {
 	*(int*) (DECON_F_BASE + HW_SW_TRIG_CONTROL) = 0x1281;
 	return 0;
 }
 
 #ifdef CONFIG_SIMPLE_FB
-static struct video_info a127f_fb = {
+static struct video_info a12s_fb = {
 	.format = FB_FORMAT_ARGB8888,
 	.width = 720,
 	.height = 1600,
@@ -25,19 +25,19 @@ static struct video_info a127f_fb = {
 };
 #endif
 
-int a127f_drv(void)
+int a12s_drv(void)
 {
 #ifdef CONFIG_SIMPLE_FB
-	REGISTER_DRIVER("simplefb", simplefb_probe, &a127f_fb);
+	REGISTER_DRIVER("simplefb", simplefb_probe, &a12s_fb);
 #endif
 	return 0;
 }
 
 struct board_data board_ops = {
-	.name = "samsung-a127f",
+	.name = "samsung-a12s",
 	.ops = {
-		.early_init = a127f_init,
-		.drivers_init = a127f_drv,
+		.early_init = a12s_init,
+		.drivers_init = a12s_drv,
 	},
 	.quirks = 0
 };
