099fb4edb84df2fe6e5784f419b3da5a1c9d6a75
oboev <122733416+oboev@users.noreply.github.com>
2026-08-03 10:28:08 +0100

    board: add support for OnePlus 13r (giulia)

---
+++ board/oneplus/board-giulia.c
@@ -0,0 +1,32 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2026, Aleksey Solovyev <avsoloviev@gmail.com>
+ */
+
+#include <board.h>
+#include <util.h>
+#include <drivers/framework.h>
+#include <lib/debug.h>
+#include <lib/simplefb.h>
+
+static struct video_info giulia_fb = {
+	.format = FB_FORMAT_ARGB8888,
+	.width = 1264,
+	.height = 2780,
+	.stride = 4,
+	.address = (void *)0xd5100000,
+	.scale = 2
+};
+
+static const struct device giulia_devices[] = {
+	{ "simplefb", &giulia_fb, "fb" },
+};
+
+struct board_data board_ops = {
+	.name = "oneplus-giulia",
+	.ops = {
+	},
+	.devices = giulia_devices,
+	.num_devices = ARRAY_SIZE(giulia_devices),
+	.quirks = 0
+};
